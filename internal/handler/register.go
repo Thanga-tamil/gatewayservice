22563312715ad0d2a4177df581e00b6991a458a0
thangatamil <thangatamil518@gmail.com>
2026-08-11 19:19:14 +0530

    AES-256

--- internal/handler/register.go
+++ internal/handler/register.go
@@ -1,8 +1,29 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"gateway/internal/encrypt"
+
+	"github.com/gin-gonic/gin"
+)
+
+type user struct{
+	UserId string
+	Name string
+	mobileNumber int64
+}
 
 func RegisterUser(ginCtx *gin.Context){
+	var User user
+	if err := ginCtx.ShouldBindJSON(&User); err != nil {
+		panic(err)
+	}
+	fmt.Println("user: ", User)
+	decryptedData, err := encrypt.AesDecrypt("", "", "")
+	if err != nil {
+		panic(err)
+	}
 
+	fmt.Println("decryptedData: ", string(decryptedData))
 }
 
